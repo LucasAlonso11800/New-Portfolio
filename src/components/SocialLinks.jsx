594b2a9cf316d9458e4e24a312493513d4a6b481
Lucas <lucasalonso11800@gmail.com>
2021-05-26 21:10:45 -0300

    Added Gmail

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -6,38 +6,41 @@ import { socialLinks } from "../portfolio";
 
 const SocialLinks = () => {
     return (
-        <div className="btn-wrapper text-lg">
-            <Button
-                className="btn-icon-only rounded-circle ml-1"
-                color="github"
-                href={socialLinks.github}
-                target="_blank"
-            >
-                <span className="btn-inner--icon">
-                    <i className="fa fa-github" />
-                </span>
-            </Button>
-            <Button
-                className="btn-icon-only rounded-circle ml-1"
-                color="instagram"
-                href={socialLinks.gmail}
-                target="_blank"
-            >
-                <span className="btn-inner--icon">
-                    <i className="fa fa-google" />
-                </span>
-            </Button>
-            <Button
-                className="btn-icon-only rounded-circle ml-1"
-                color="twitter"
-                href={socialLinks.linkedin}
-                target="_blank"
-            >
-                <span className="btn-inner--icon">
-                    <i className="fa fa-linkedin" />
-                </span>
-            </Button>
-        </div>
+        <>
+            <div className="btn-wrapper text-lg">
+                <Button
+                    className="btn-icon-only rounded-circle ml-1"
+                    color="github"
+                    href={socialLinks.github}
+                    target="_blank"
+                >
+                    <span className="btn-inner--icon">
+                        <i className="fa fa-github" />
+                    </span>
+                </Button>
+                <Button
+                    className="btn-icon-only rounded-circle ml-1"
+                    color="instagram"
+                    href={socialLinks.gmail}
+                    target="_blank"
+                >
+                    <span className="btn-inner--icon">
+                        <i className="fa fa-google" />
+                    </span>
+                </Button>
+                <Button
+                    className="btn-icon-only rounded-circle ml-1"
+                    color="twitter"
+                    href={socialLinks.linkedin}
+                    target="_blank"
+                >
+                    <span className="btn-inner--icon">
+                        <i className="fa fa-linkedin" />
+                    </span>
+                </Button>
+            </div>
+            <h6 className="mt-4 text-white">lucasalonso11800@gmail.com</h6>
+        </>
     );
 }
 
